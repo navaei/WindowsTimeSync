d7dfa8721169f6cc73de46e36502d1647348c754
Meysam Navaei <meysam.navaei@hotmail.com>
2025-08-05 21:58:52 +0330

    modify: add SetTimeByKeybit

---
+++ WindowsTimeSetter/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsTimeSync
+{
+    internal class Constants
+    {
+        public const double UtcSubtractHours = -3.5;
+        public static DateTime StartUnixDate => new DateTime(1970, 1, 1);
+
+    }
+}
