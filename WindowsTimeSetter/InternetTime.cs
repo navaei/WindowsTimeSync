3c59fbcfe522361b531884629dbf3653b152b83f
Meysam Navaei <meysam.navaei@gmail.com>
2025-08-05 21:50:44 +0330

    modify: add intenet time zone

--- WindowsTimeSetter/InternetTime.cs
+++ WindowsTimeSetter/InternetTime.cs
@@ -9,4 +9,24 @@ namespace WindowsTimeSync
         public string client_ip { get; set; }
         public DateTime datetime { get; set; }
     }
+
+    public class InternetTimeZone
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public string countryCode { get; set; }
+        public string countryName { get; set; }
+        public string regionName { get; set; }
+        public string cityName { get; set; }
+        public string zoneName { get; set; }
+        public string abbreviation { get; set; }
+        public int gmtOffset { get; set; }
+        public string dst { get; set; }
+        public int zoneStart { get; set; }
+        public object zoneEnd { get; set; }
+        public object nextAbbreviation { get; set; }
+        public int timestamp { get; set; }
+        public string formatted { get; set; }
+    }
+
 }
--- WindowsTimeSetter/Program.cs
+++ WindowsTimeSetter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
@@ -9,6 +10,9 @@ namespace WindowsTimeSync
     class Program
     {
         const string defaultZone = "Asia/Tehran";
+        const string lat = "35.6944";
+        const string lng = "51.4215";
+        const string apiKey = "V2ZJELAQIGBW";
         static System.Timers.Timer timer = new System.Timers.Timer(90000);
 
         static void Main(string[] args)
@@ -19,13 +23,16 @@ namespace WindowsTimeSync
             if (args.Any())
                 localZone = args[0].Trim();
 
-            timer.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => SetTime(localZone);
+            if (string.IsNullOrEmpty(lat))
+                timer.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => SetTimeByWorldTimeApi(localZone);
+            else
+                timer.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => SetTimeByTimeZonedb(localZone, lat, lng);
             timer.Start();
 
             Console.ReadKey();
         }
 
-        private static void SetTime(string localZone)
+        private static void SetTimeByWorldTimeApi(string localZone)
         {
             try
             {
@@ -33,12 +40,12 @@ namespace WindowsTimeSync
                 var localDateUtc = DateTime.Now;
 
                 var timeDate = new HttpClient().GetStringAsync("http://worldtimeapi.org/api/timezone/" + localZone).Result;
-                var nowDate = JsonSerializer.Deserialize<InternetTime>(timeDate);
-                if ((string.IsNullOrEmpty(localZone) && Math.Abs(nowDate.datetime.Subtract(localDateUtc).TotalSeconds) > 3) ||
-                    (!string.IsNullOrEmpty(localZone) && Math.Abs(nowDate.datetime.Subtract(localDate).TotalSeconds) > 3))
+                var serverDate = JsonSerializer.Deserialize<InternetTime>(timeDate);
+                if ((string.IsNullOrEmpty(localZone) && Math.Abs(serverDate.datetime.Subtract(localDateUtc).TotalSeconds) > 3) ||
+                    (!string.IsNullOrEmpty(localZone) && Math.Abs(serverDate.datetime.Subtract(localDate).TotalSeconds) > 3))
                 {
-                    WinDateTime.SetDateTime(nowDate.datetime, localZone);
-                    Console.WriteLine(DateTime.Now.ToString("t") + ": Set correct time: " + nowDate.datetime.ToString("t"));
+                    WinDateTime.SetDateTime(serverDate.datetime, localZone);
+                    Console.WriteLine(DateTime.Now.ToString("t") + ": Set correct time: " + serverDate.datetime.ToString("t"));
                 }
                 else
                 {
@@ -54,6 +61,50 @@ namespace WindowsTimeSync
                     timer.Stop();
                 }
             }
+        }
+
+        private static void SetTimeByTimeZonedb(string localZone, string lat, string lng)
+        {
+            try
+            {
+                var localDate = DateTime.Now;
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                var timeDate = new HttpClient().GetStringAsync($"https://api.timezonedb.com/v2.1/get-time-zone?key={apiKey}&format=json&by=position&lat={lat}&lng={lng}").Result;
+                stopWatch.Stop();
+                if (stopWatch.Elapsed.TotalSeconds > 2)
+                {
+                    Console.WriteLine("so slowly! " + stopWatch.Elapsed.TotalSeconds);
+                    return;
+                }
+
+                localDate = localDate.AddMilliseconds(-500);
+                var result = JsonSerializer.Deserialize<InternetTimeZone>(timeDate);
+                var serverDate = DateTime.Parse(result.formatted);
+                if (Math.Abs(serverDate.Subtract(localDate).TotalSeconds) > 3)
+                {
+                    WinDateTime.SetDateTime(serverDate, localZone);
+                    Console.WriteLine(DateTime.Now.ToString("t") + ": Set correct time: " + serverDate.ToString("t"));
+                }
+                else
+                {
+                    Console.WriteLine(DateTime.Now.ToString("t") + " No changes detected");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                if (ex.Message.Contains("A required privilege is not held by the client"))
+                {
+                    Console.WriteLine("\n---------\nPlease run program as administrator\n");
+                    timer.Stop();
+                }
+            }
+        }
+
+        public static DateTime GetDate(int datetime)
+        {
+            return new DateTime(1970, 1, 1).AddSeconds(datetime).Add(DateTime.Now.Subtract(DateTime.UtcNow));
         }
     }
 }
